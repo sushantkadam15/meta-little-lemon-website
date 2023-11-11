c93d10e0cb4996ff528f2e43877dabaf8f1af094
Sushant Kadam <sushantkadam15@gmail.com>
2023-11-11 07:29:17 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <section className=" flex h-screen flex-col items-center justify-center">
+      <h1 className=" text-4xl">Hello From Tailwind CSS 👋🏻</h1>
+      <footer className="fixed bottom-0 mb-5 tracking-wide text-zinc-600 ">
+        Template By{"  "}
+        <a
+          className=" text-violet-600"
+          href="https://www.linkedin.com/in/sushant-p-kadam/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sushant
+        </a>
+      </footer>
+    </section>
+  );
+}
+
+export default App;
